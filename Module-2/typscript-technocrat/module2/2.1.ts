ba3cc950621fc5e035a421115296d3899304ecf5
lanjudev16 <lanjudev16@gmail.com>
2023-11-03 22:28:56 +0600

    Feat:type assertion image added

--- Module-2/typscript-technocrat/module2/2.1.ts
+++ Module-2/typscript-technocrat/module2/2.1.ts
@@ -1,6 +1,17 @@
-let anything:any
-anything="Next level developer"
-anything=true;
+let anything: any
+anything = "Next level developer"
+anything = true;
 (anything as boolean)
 
-const kgToGm=
+const kgToGm = (value: string | number) => {
+    if (typeof value === "string") {
+        let convertedValue: number;
+        convertedValue = parseFloat(value) * 1000
+        return `The converted value is ${convertedValue}`
+    }
+    if (typeof value === "number") {
+        return value * 1000
+    }
+}
+const result1=kgToGm("1") as string
+const result2=kgToGm(1) as number
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 ## Typescript assertion
 When I am sure what the type of the variable or function what the return  but the typescript engine can  not say actual type then it is called type assertion
 
+![alt text for screen readers](https://i.ibb.co/qY3HF21/assertion.png "Text to show on mouseover").
 
